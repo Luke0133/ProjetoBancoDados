8779ca0ef17abc5a587dc7935d1ab918181a88c6
SirSdyro <136931615+SirSdyro@users.noreply.github.com>
2025-06-29 13:48:02 -0300

    Proposta de função para execução dos comandos sql

--- App/db/database.py
+++ App/db/database.py
@@ -3,34 +3,44 @@ import psycopg2.extras #Esse modulo permite a gente obter um dicionario quando f
 #dai ao inves de precisarmos saber qual a posicao da coluna na lista, podemos so fazer
 #"nome_variavel"["name"], embaixo, dentro do try, tem um exemplo.
 
-hostname = 'localhost'
-database = 'BDExtensao'
-username = 'postgres'
-pwd = 'adminext'
-port_id = 5432
-conn = None
-cur = None
+def comandoSQL(comando):
+    hostname = 'localhost'
+    database = 'BDExtensao'
+    username = 'postgres'
+    pwd = 'adminext'
+    port_id = 5432
+    conn = None
+    cur = None
 
-try:
-    conn = psycopg2.connect(
-        host = hostname,
-        dbname = database,
-        user = username,
-        password = pwd,
-        port = port_id
-    )
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    try:
+        conn = psycopg2.connect(
+            host = hostname,
+            dbname = database,
+            user = username,
+            password = pwd,
+            port = port_id
+        )
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    #exemplo
-    cur.execute("SELECT * FROM tb_aluno")
-    for record in cur.fetchall():
-        print(record['dataingresso'])
+        #Tratamendo dos comandos SQL
+        cur.execute(comando)
+        if comando[:3].lower() == "sel":
+            return cur.fetchall()
+        elif comando[:3].lower() == "upd":
+            conn.commit()
+            print("Valor(es) atualizados com sucesso")
+        elif comando[:3].lower() == "ins":
+            conn.commit()
+            print("Valor(es) inseridos com sucesso")
+        elif comando[:3].lower() == "del":
+            conn.commit()
+            print("Registro(s) deletado(s) com sucesso")
+            
 
-
-except Exception as error:
-    print(error)
-finally:
-    if cur is not None:
-        cur.close()
-    if conn is not None:
-        conn.close()
+    except Exception as error:
+        print(error)
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
