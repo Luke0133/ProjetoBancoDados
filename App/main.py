7733bda5347e6609ac01ed297ab3cdca7bbb1db9
SirSdyro <136931615+SirSdyro@users.noreply.github.com>
2025-06-29 13:54:54 -0300

    Código para rodar o processo de login

--- App/main.py
+++ App/main.py
@@ -3,3 +3,11 @@ from ui.gui import App
 if __name__ == "__main__":
     app = App()
     app.run()
+
+#Para executar o login process, use este código
+'''
+from ui.gui import loginProcess
+
+if __name__ == "__main__":
+    loginProcess()
+'''
